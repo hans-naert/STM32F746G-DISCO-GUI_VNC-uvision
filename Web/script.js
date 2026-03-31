3d8a9649c946dd13d3543e75423b831e187a8ad0
Hans Naert <hans.naert@vives.be>
2026-03-31 10:39:45 +0200

    Update checkbox vanuit JavaScript code

--- Web/script.js
+++ Web/script.js
@@ -2,5 +2,11 @@ window.setInterval(async () => {
     console.log("Hello world");
     let response = await fetch("button.cgi");
     let text = await response.text();
+    if(text=="checked") {
+        document.getElementById("myCheckbox").setAttribute('checked',''); 
+    }
+    else {
+        document.getElementById("myCheckbox").removeAttribute('checked');
+    }        
     console.log(text);
 }, 3000);
