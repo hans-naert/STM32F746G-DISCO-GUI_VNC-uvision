86b61cfa570e7e555eabb9336840e0faff96aea3
Hans Naert <hans.naert@vives.be>
2026-03-31 10:18:04 +0200

    fetch button status from button.cgi and write on console

--- Web/script.js
+++ Web/script.js
@@ -1,3 +1,6 @@
-window.setInterval(() => {
+window.setInterval(async () => {
     console.log("Hello world");
+    let response = await fetch("button.cgi");
+    let text = await response.text();
+    console.log(text);
 }, 3000);
